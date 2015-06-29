bd8da88b20cf43e3ad2812797ca3b5b38fd27245
Nathen Harvey <nharvey@chef.io>
2015-06-29 17:25:41 +0000

    Update the apache server recipe

--- cookbooks/apache/recipes/server.rb
+++ cookbooks/apache/recipes/server.rb
@@ -2,10 +2,23 @@ package 'httpd' do
   action :install
 end
 
+# Create a web user and group
+user 'web'
+group 'web' 
+
 file '/var/www/html/index.html' do
   content '<h1>Hello, world!</h1>'
   action :create
+  user 'web'
+  group 'web'
 end
+
+# Attempt to start a bogus service, ignore the failure
+service 'foo' do
+  action :start
+  ignore_failure true
+end
+
 
 service 'httpd' do
   action [:start, :enable]
